85cb38006ccff916147fb9b39346c5710b3ea139
nhatnh <nhatnhse05539@gmail.com>
2020-07-09 19:42:04 +0700

    #commit account setting

--- src/components/pages/AccountSetting.js
+++ src/components/pages/AccountSetting.js
@@ -9,16 +9,34 @@ import '../../css/Section.css';
 import Loader from '../common/LoaderVer2';
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
+import DatePicker from 'react-datepicker';
+import "react-datepicker/dist/react-datepicker.css";
+import { toast } from 'react-toastify';
 const isEmpty = require("is-empty");
 class AccountSetting extends Component {
     constructor(props) {
         super(props);
         this.state = {
             userInfo: {},
+            userInfoUpdate: {
+                firstName: '',
+                lastName: '',
+                fullName: '',
+                avatar: '',
+                email: '',
+                birthday: new Date(),
+                gender: 'Khác',
+                description: '',
+                posts: 0,
+                views: 0,
+                likes: 0,
+                createAt: ''
+            },
             posts: [],
             top: [],
             nextPage: 2,
-            loading: false
+            loading: false,
+            errors: {}
         };
         this.showMore = this.showMore.bind(this);
     }
@@ -33,6 +51,65 @@ class AccountSetting extends Component {
             month = today.getMonth() + 1
         }
         return today.getDate() + "/" + month + "/" + today.getFullYear();
+    }
+
+    onDateChange = date => {
+        this.setState({
+            userInfoUpdate: { ...this.state.userInfoUpdate, birthday: date }
+        })
+    }
+
+    onFirstNameChange = e => {
+        this.setState({
+            userInfoUpdate: { ...this.state.userInfoUpdate, firstName: e.target.value }
+        })
+    }
+
+    onLastNameChange = e => {
+        this.setState({
+            userInfoUpdate: { ...this.state.userInfoUpdate, lastName: e.target.value }
+        })
+    }
+
+    onGenderChange = e => {
+        this.setState({
+            userInfoUpdate: { ...this.state.userInfoUpdate, gender: e.target.value }
+        })
+    }
+
+    onDescriptionChange = e => {
+        this.setState({
+            userInfoUpdate: { ...this.state.userInfoUpdate, description: e.target.value }
+        })
+    }
+
+    handleSubmit = e => {
+        e.preventDefault();
+        let formData = new FormData();
+        formData.append('firstName', this.state.userInfoUpdate.firstName);
+        formData.append('lastName', this.state.userInfoUpdate.lastName);
+        formData.append('birthday', this.state.userInfoUpdate.birthday);
+        formData.append('description', this.state.userInfoUpdate.description);
+        formData.append('gender', this.state.userInfoUpdate.gender);
+        axios
+            .post("http://178.128.83.129:3000/api/users", formData)
+            .then(res => {
+                toast.success('Save successfully!', { position: toast.POSITION.TOP_RIGHT });
+                axios.get("http://178.128.83.129:3000/api/users/" + this.props.auth.user.id)
+                    .then(resp => this.setState({
+                        userInfo: resp.data.user,
+                    })).catch(errs =>
+                        console.log(errs)
+                    );
+            })
+            .catch(err => {
+                console.log(err);
+                console.log(err.response.data);
+                this.setState({
+                    errors: err.response.data
+                })
+            }
+            );
     }
 
     showMore(nextPage) {
@@ -61,6 +138,7 @@ class AccountSetting extends Component {
                 if (this.mounted) {
                     this.setState({
                         userInfo: res[0].data.user,
+                        userInfoUpdate: res[0].data.user,
                         posts: res[1].data.allPosts,
                         top: res[2].data.topPost,
                         loading: false
@@ -85,11 +163,11 @@ class AccountSetting extends Component {
                         <div className="col-md-4 asetting-info-container">
                             <Avatar className="asetting-avatar" image={this.state.userInfo.avatar} size={140} tooltip={false} />
                             <div className="asetting-name">{this.state.userInfo.fullName}</div>
-                            <div className="asetting-smalltext">Giới tính: {this.state.userInfo.gender}</div>
-                            <div className="asetting-smalltext">Ngày sinh: {this.state.userInfo.birthday}</div>
-                            <div className="asetting-num"><div className="asetting-left"><img className="asetting-icon" width={20} src="/images/post.png" /> &nbsp;Bài viết:</div><div className="asetting-right">{this.state.userInfo.posts}</div></div>
-                            <div className="asetting-views"><div className="asetting-left"><img className="asetting-icon" width={20} src="/images/eye.png" /> &nbsp;Lượt xem:</div> <div className="asetting-right">{this.state.userInfo.views}</div></div>
-                            <div className="asetting-likes"><div className="asetting-left"><img className="asetting-icon" width={20} src="/images/like.png" /> &nbsp;Lượt thích:</div> <div className="asetting-right">{this.state.userInfo.likes}</div></div>
+                            <div className="asetting-smalltext"><i className="fas fa-venus-mars"></i> Giới tính: {this.state.userInfo.gender}</div>
+                            <div className="asetting-smalltext"><i className="far fa-calendar-alt"></i> Ngày sinh: {this.getFormattedDate(this.state.userInfo.birthday)}</div>
+                            <div className="asetting-num"><div className="asetting-left"><i className="far fa-file-alt asetting-icon"></i>Bài viết:</div><div className="asetting-right">{this.state.userInfo.posts}</div></div>
+                            <div className="asetting-views"><div className="asetting-left"><i className="far fa-eye asetting-icon"></i>Lượt xem:</div> <div className="asetting-right">{this.state.userInfo.views}</div></div>
+                            <div className="asetting-likes"><div className="asetting-left"><i className="far fa-heart asetting-icon"></i>Lượt thích:</div> <div className="asetting-right">{this.state.userInfo.likes}</div></div>
                             <div className="asetting-smalltext-end">Gia nhập kể từ ngày: 01/07/2020</div>
                             <div className="asetting-line"></div>
                             <div className="asetting-tab">Thông tin tài khoản</div>
@@ -104,41 +182,47 @@ class AccountSetting extends Component {
                                 Nhớ bấm lưu trước khi rời khỏi trang nhé
                             </div>
                             <form className="create-form" onSubmit={(e) => this.handleSubmit(e)}>
-                                <div className="form-group create-form-group">
-                                    <input readOnly autoComplete="off" maxLength="100" onChange={this.onChange} id="email" value={this.state.userInfo.email} type="text" className="form-control create-input-name" placeholder=" " />
-                                    <label className="create-label-name" for="email">Email</label>
+                                <div className="form-group asetting-form-group">
+                                    <input readOnly autoComplete="off" maxLength="100" id="email" value={this.state.userInfoUpdate.email} type="text" className="form-control asetting-input-email" placeholder=" " />
+                                    <label className="asetting-label-email" htmlFor="email">Email</label>
                                 </div>
-                                <div className="form-group create-form-group">
-                                    <input autoComplete="off" maxLength="100" onChange={this.onChange} id="firstName" value={this.state.userInfo.firstName} type="text" className="form-control create-input-name" placeholder=" " />
-                                    <label className="create-label-name" for="firstName">Họ</label>
+                                <div className="form-group asetting-form-group">
+                                    <input autoComplete="off" maxLength="100" onChange={this.onFirstNameChange} id="firstName" value={this.state.userInfoUpdate.firstName} type="text" className="form-control asetting-input-name" placeholder=" " />
+                                    <label className="asetting-label-name" htmlFor="firstName">Họ</label>
                                 </div>
-                                <div className="form-group create-form-group">
-                                    <input autoComplete="off" maxLength="100" onChange={this.onChange} id="lastName" value={this.state.userInfo.lastName} type="text" className="form-control create-input-name" placeholder=" " />
-                                    <label className="create-label-name" for="lastName">Tên</label>
+                                <div className="form-group asetting-form-group">
+                                    <input autoComplete="off" maxLength="100" onChange={this.onLastNameChange} id="lastName" value={this.state.userInfoUpdate.lastName} type="text" className="form-control asetting-input-name" placeholder=" " />
+                                    <label className="asetting-label-name" htmlFor="lastName">Tên</label>
                                 </div>
-                                <div className="form-group create-dropdown">
-                                    <select onChange={this.onChange} className="form-control create-form-group create-dropdown-color" value='' id="dropdown_value">
-                                        <option className="create-option" disabled defaultValue hidden value='' >Giới tính</option>
-                                        {
-                                            this.state.category && this.state.category.map((x, i) =>
-                                                <option className="create-option" key={i} value={x._id}>{x.title}</option>
-                                            )
-                                        }
-
+                                <div className="form-group asetting-dropdown">
+                                    <label className="asetting-label-gender" htmlFor="dropdown_value">Giới tính</label>
+                                    <select onChange={this.onGenderChange} className="form-control asetting-dropdown-color" value={this.state.userInfoUpdate.gender} id="dropdown_value">
+                                        <option className="asetting-option" value='Nam'>Nam</option>
+                                        <option className="asetting-option" value='Nữ'>Nữ</option>
+                                        <option className="asetting-option" value='Khác'>Khác</option>
                                     </select>
                                 </div>
-                                <div className="form-group create-form-group">
-                                    <input autoComplete="off" maxLength="100" onChange={this.onChange} id="title" type="text" className="form-control create-input-name" placeholder=" " />
-                                    <label className="create-label-name" for="title">Ngày sinh</label>
+                                <div className="form-group asetting-form-group">
+                                    <label className="asetting-label-date" htmlFor="title">Ngày sinh</label>
+                                    <div className="asetting-datepicker">
+                                        <DatePicker
+                                            onChange={this.onDateChange}
+                                            selected={new Date(this.state.userInfoUpdate.birthday)}
+                                            showMonthDropdown
+                                            showYearDropdown
+                                            dropdownMode="select"
+                                        />
+                                    </div>
                                 </div>
-                                <div className="form-group create-form-group">
-                                    <textarea spellCheck="false" maxLength="1000" onChange={this.onChange} id="description" style={{ resize: 'none' }} value={this.state.description}
-                                        className="form-control create-input-description" rows="1" placeholder=" "></textarea>
-                                    <label className="create-label-name" for="description">Mô tả về bản thân</label>
-                                </div>
+                                <div className="form-group asetting-form-group">
+                                    <label className="asetting-label-description" htmlFor="description">Mô tả về bản thân</label>
+                                    <textarea spellCheck="false" maxLength="1000" onChange={this.onDescriptionChange} id="description" style={{ resize: 'none' }} value={this.state.userInfoUpdate.description}
+                                        className="form-control" rows="3" placeholder=" "></textarea>
 
-                                <div className="create-button-container">
-                                    <button type="submit" className="btn btn-pink">Lưu</button>
+                                </div>
+                                {!isEmpty(this.state.errors) && <div className="alert alert-danger">{this.state.errors.message}</div>}
+                                <div className="asetting-button-container">
+                                    <button type="submit" className="btn btn-pink" onClick={(e) => this.handleSubmit(e)}>Lưu</button>
                                 </div>
                             </form>
                         </div>
