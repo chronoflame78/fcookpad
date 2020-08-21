75d6b37b163937d59197919d0923b6504ccba86b
Le Hoang Long - K11 FUG HN <longlhse04894@fpt.edu.vn>
2020-08-21 03:41:36 +0000

    Revert "#update verifyAccount"
    This reverts commit 1cea451e6ad89bc9a9f9201173dc3d8fd15a498a

--- src/components/auth/VerifyAccount.js
+++ src/components/auth/VerifyAccount.js
@@ -1,149 +1,128 @@
 import React, { Component } from "react";
-import "../../css/Confirm.css";
+import '../../css/Confirm.css';
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
-import { Link } from "react-router-dom";
-import axios from "axios";
-import {apiURL} from "../../config/Constant";
+import { verifyAccount } from "../../actions/authActions";
+import Page404 from '../pages/error/Page404';
+import { Link } from 'react-router-dom';
+
+const isEmpty = require("is-empty");
+
 
 class VerifyAccount extends Component {
   constructor(props) {
+    console.log("post constructor");
     super(props);
     this.state = {
-      isVerify: false,
-      isError: false
+      verify: false,
+      errors: {}
     };
   }
 
   componentDidMount() {
     // If logged in and user navigates to Login page, should redirect them to home
-    if (this.props.auth.isAuthenticated) {
-      this.props.history.push("/");
-    } else {
-      let data = {
-        token: this.props.match.params.token
-      }
-      axios
-        .post(`${apiURL}/auth/verify`, data)
-        .then((res) => {
-          // Set token to localStorage
-          const { token } = res.data;
-          localStorage.setItem("jwtToken", token);
-          this.setState({
-            isVerify: true
-          })
-        })
-        .catch((err) =>{
-          console.log(err);
-          this.setState({
-            isError: true
-          })
-        });
+    // if (this.props.auth.isAuthenticated) {
+    //   this.props.history.push("/");
+    // }
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.auth.isAuthenticated) {
+      this.setState({
+        verify: true,
+        errors: {}
+      })
+    }
+    if (nextProps.errors) {
+      this.setState({
+        errors: nextProps.errors
+      });
     }
   }
 
   render() {
-    if (this.state.isError) {
+    if (!isEmpty(this.state.errors)) {
       return (
         <div className="parent-content">
-          <div className="verify-content-wrapper">
-            <div className="form-wrapper">
-              <div className="back-container">
-                <Link to="/">
-                  <img
-                    src="/images/back-arrow.png"
-                    alt="Back"
-                    width="16.91"
-                    height="12.3"
-                  />
-                </Link>
-              </div>
-              <div className="justify-content">
-                <div className="main-content">
-                  <div className="logo-container">
-                    <img
-                      src="/images/NewLogo.png"
-                      alt="Back"
-                      width="122"
-                      height="100"
-                    />
-                  </div>
-                  <div className="oms-title">
-                    <h1>
-                      <b>Lỗi!</b>
-                    </h1>
-                  </div>
-                  <div className="text-container">
-                    <label>Token không đúng hoặc đã hết hạn.</label>
-                  </div>
+        <div className="verify-content-wrapper">
+          <div className="form-wrapper">
+            <div className="back-container">
+              <Link to="/">
+                <img src="/images/back-arrow.png" alt="Back" width="16.91" height="12.3" />
+              </Link>
+            </div>
+            <div className="justify-content">
+              <div className="main-content">
+                <div className="logo-container">
+                  <img src="/images/NewLogo.png" alt="Back" width="122" height="100" />
+                </div>
+                <div className="oms-title">
+                  <h1><b>Lỗi!</b></h1>
+                </div>
+                <div className="text-container">
+                  <label>Token không đúng hoặc đã hết hạn.
+                            </label>
                 </div>
               </div>
             </div>
           </div>
         </div>
+      </div>
       );
+
     }
-    else if(this.state.isVerify === true){
-      return (
-        <div className="parent-content">
-          <div className="verify-content-wrapper">
-            <div className="form-wrapper">
-              <div className="back-container">
-                <a href="/">
-                  <img
-                    src="/images/back-arrow.png"
-                    alt="Back"
-                    width="16.91"
-                    height="12.3"
-                  />
-                </a>
-              </div>
-              <div className="justify-content">
-                <div className="main-content">
-                  <div className="logo-container">
-                    <img
-                      src="/images/NewLogo.png"
-                      alt="Back"
-                      width="122"
-                      height="100"
-                    />
-                  </div>
-                  <div className="oms-title">
-                    <h1>
-                      <b>Cứ tự nhiên nhé bếp trưởng!</b>
-                    </h1>
-                  </div>
-                  <div className="text-container">
-                    <label>
-                      Bạn đã đăng ký thành công, bắt đầu chia sẻ công thức của
-                      mình nào.
-                    </label>
-                  </div>
-                  <div className="resend-btn-container">
-                    <a href="/">
-                      <button className="btn btn-pink btn-login">
-                        Quay về trang chủ
-                      </button>
-                    </a>
-                  </div>
+    if (this.state.verify === false) {
+      const verifyToken = {
+        token: this.props.match.params.token
+      };
+      this.props.verifyAccount(verifyToken);
+      return (<div></div>);
+    }
+    return (
+      <div className="parent-content">
+        <div className="verify-content-wrapper">
+          <div className="form-wrapper">
+            <div className="back-container">
+              <Link to="/">
+                <img src="/images/back-arrow.png" alt="Back" width="16.91" height="12.3" />
+              </Link>
+            </div>
+            <div className="justify-content">
+              <div className="main-content">
+                <div className="logo-container">
+                  <img src="/images/NewLogo.png" alt="Back" width="122" height="100" />
+                </div>
+                <div className="oms-title">
+                  <h1><b>Cứ tự nhiên nhé bếp trưởng!</b></h1>
+                </div>
+                <div className="text-container">
+                  <label>Bạn đã đăng ký thành công, bắt đầu chia sẻ công thức của mình nào.
+                            </label>
+                </div>
+                <div className="resend-btn-container">
+                  <Link to="/">
+                    <button className="btn btn-pink btn-login">Quay về trang chủ</button>
+                  </Link>
                 </div>
               </div>
             </div>
           </div>
         </div>
-      );
-    }
-    else{
-      return <div></div>;
-    }
-    
+      </div>
+    );
   }
 }
 
 VerifyAccount.propTypes = {
-  auth: PropTypes.object.isRequired
+  verifyAccount: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+  errors: PropTypes.object.isRequired
 };
-const mapStateToProps = (state) => ({
+const mapStateToProps = state => ({
   auth: state.auth,
+  errors: state.errors
 });
-export default connect(mapStateToProps)(VerifyAccount);
+export default connect(
+  mapStateToProps,
+  { verifyAccount }
+)(VerifyAccount);
