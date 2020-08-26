6212603327da49589a46e790c2df28f1c4df60e3
daholakid <45927445+daholakid@users.noreply.github.com>
2020-08-26 20:16:14 +0700

    Merge branch 'master' of https://gitlab.com/dungbkvn1998/capstone_project_frontend into long.lhoang

---
+++ src/components/pages/Policy.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import Footer from "../layout/Footer";
+
+export default class Policy extends React.Component {
+    render() {
+        return(
+<div>
+    POLICY PAGE
+</div>
+        )
+    }
+}
+
