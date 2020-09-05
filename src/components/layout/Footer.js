191b936ddfb879a4389162ad6118c6918d7c8bcf
Nguyễn Việt Dũng <dung.nguyen@adamodigital.com>
2020-09-05 21:07:28 +0700

    update footer

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -3,10 +3,10 @@ import '../../css/Footer.css';
 
 export default class Footer extends React.Component {
 
-render(){
-    return(
-        <footer className="footer">
-        {/* <div class="footer-above-content">
+    render() {
+        return (
+            <footer className="footer">
+                {/* <div class="footer-above-content">
             <div class="footer-text-and-btn">
                 <div class="footer-inner-content">
                     <div class="footer-text">
@@ -21,52 +21,52 @@ render(){
                 </div>
             </div>
         </div> */}
-        <div className="footer-under-content">
-            <div className="footer-logo-container">
-                <img className="footer-logo" alt="" src="/images/Logo.png"/>
-            </div>
-            <div className="footer-contact-cont">
-                <div className="footer-social-outer-cont">
-                    <ul className="footer-social-container">
-                        <li>
-                            <a href="/">
-                                <img  className="footer-fb-icon-cont" alt="" src="/images/facebook.png"/>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="/">
-                                <img  className="footer-yt-icon-cont" alt="" src="/images/youtube.png"/>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="/">
-                                <img  className="footer-ig-icon-cont" alt="" src="/images/instagram.png"/>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="/">
-                                <img  className="footer-pr-icon-cont" alt="" src="/images/pinterest.png"/>
-                            </a>
-                        </li>
-                    </ul>
+                <div className="footer-under-content">
+                    <div className="footer-logo-container">
+                        <img className="footer-logo" alt="" src="/images/Logo.png" />
+                    </div>
+                    <div className="footer-contact-cont">
+                        {/* <div className="footer-social-outer-cont">
+                            <ul className="footer-social-container">
+                                <li>
+                                    <a href="/">
+                                        <img className="footer-fb-icon-cont" alt="" src="/images/facebook.png" />
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="/">
+                                        <img className="footer-yt-icon-cont" alt="" src="/images/youtube.png" />
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="/">
+                                        <img className="footer-ig-icon-cont" alt="" src="/images/instagram.png" />
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="/">
+                                        <img className="footer-pr-icon-cont" alt="" src="/images/pinterest.png" />
+                                    </a>
+                                </li>
+                            </ul>
+                        </div> */}
+                        <div className="footer-mail-cont">mlemmlem0520@gmail.com</div>
+                        {/* <div className="footer-bottom-links">
+                            <div>
+                                <a href="/policy">Policy</a>
+                            </div>
+                            <div className="footer-mid-link">
+                                <a href="/">About us</a>
+                            </div>
+                            <div>
+                                <a href="/">Terms</a>
+                            </div>
+                        </div> */}
+                    </div>
                 </div>
-                <div className="footer-mail-cont">mlemmlem@gmail.com</div>
-                <div className="footer-bottom-links">
-                    <div>
-                        <a href="/policy">Policy</a>
-                    </div>
-                    <div className="footer-mid-link">
-                        <a href="/">About us</a>   
-                    </div>
-                    <div>
-                        <a href="/">Terms</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </footer>
+            </footer>
 
-    );
-}
-        
+        );
+    }
+
 }
