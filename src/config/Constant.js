c0a0ce698880b7810c5057f9ef48d97173bfb410
chronoflame78 <gaovanthet555@gmail.com>
2021-10-26 23:16:48 +0700

    commit update local link

--- src/config/Constant.js
+++ src/config/Constant.js
@@ -1,5 +1,6 @@
 //API URL Constants
-export const apiURL = "https://api.mlemmlem.site/api";
+// export const apiURL = "https://api.mlemmlem.site/api";
+export const apiURL = "http://localhost:2000/api";
 
 //View All Constants
 export const NEW = "MỚI NHẤT";
